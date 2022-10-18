f3ac9ad1d5b8c876eec8ee7409582f53fa945ee1
delta-12 <btesq235@gmail.com>
2022-10-18 14:15:12 -0400

    update formatting and comments in example code for workshop

--- workshop-example-sketches/Bluefruit_tiltSwitch v1.1.py
+++ workshop-example-sketches/Bluefruit_tiltSwitch v1.1.py
@@ -1,28 +1,28 @@
-''' Bluefruit_tiltSwitch
+""" Bluefruit_tiltSwitch
 Developed for The Art of Making: An Introduction to Hands-On System Design and Engineering
 University of Pittsburgh Swanson School of Engineering
 v1.1 Amelia Glenn 2/13/2022
-'''
 
-import board
-'import the pins on this board'
-import digitalio
+Last modified:
+Benjamin Esquieres 10/14/2022
+Changes:
+Formatting and comments
+"""
 
-'led connected to D9'
-led = digitalio.DigitalInOut(board.D9)
-'led is an output pin'
-led.switch_to_output()
+import board  # import the pins on this board
+import digitalio  # import access to basic digital input/output
 
-'tilt switch conneted to D10'
-switch = digitalio.DigitalInOut(board.D10)
-'switch is an input, and starts off'
-switch.switch_to_input(pull=digitalio.Pull.UP)
 
-while True:
-    'if tilt switch is vertical (on)'
-    if switch.value:
-        'turn off LED'
-        led.value=False
-    else:
-        'if not vertical, turn on LED'
-        led.value=True
+led = digitalio.DigitalInOut(board.D9)  # led is connected to pin D9
+led.switch_to_output()  # led is an output pin
+
+switch = digitalio.DigitalInOut(board.D10)  # tilt switch is conneted to pin D10
+switch.switch_to_input(
+    pull=digitalio.Pull.UP
+)  # switch is an input and is initially off
+
+while True:  # loop until program terminates, analogous to void loop() in Arduino
+    if switch.value:  # if tilt switch is vertical (on)
+        led.value = False  # turn off LED
+    else:  # else (if not vertical)
+        led.value = True  # turn on LED
