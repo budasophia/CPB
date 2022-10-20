48666d3e4ade2f5bdd30b1270f20c9aa86bcfcd4
delta-12 <btesq235@gmail.com>
2022-10-19 20:27:24 -0400

    make tiltball switch code more readable, explicit

--- workshop-example-sketches/Bluefruit_tiltSwitch_v1.1.py
+++ workshop-example-sketches/Bluefruit_tiltSwitch_v1.1.py
@@ -16,13 +16,13 @@ import digitalio  # import access to basic digital input/output
 led = digitalio.DigitalInOut(board.D9)  # led is connected to pin D9
 led.switch_to_output()  # led is an output pin
 
-switch = digitalio.DigitalInOut(board.D10)  # tilt switch is conneted to pin D10
-switch.switch_to_input(
+tiltball = digitalio.DigitalInOut(board.D10)  # tilt switch is conneted to pin D10
+tiltball.switch_to_input(
     pull=digitalio.Pull.UP
-)  # switch is an input and is initially off
+)  # tiltball is an input and is initially off
 
 while True:  # loop until program terminates, analogous to void loop() in Arduino
-    if switch.value:  # if tilt switch is vertical (on)
+    if tiltball.value == True:  # if tilt switch is vertical (closed circuit)
         led.value = False  # turn off LED
-    else:  # else (if not vertical)
+    else:  # if not vertical (open circuit)
         led.value = True  # turn on LED
