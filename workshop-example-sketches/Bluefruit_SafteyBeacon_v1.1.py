664be65220b3143206a17b117349ca3fb74932a0
delta-12 <btesq235@gmail.com>
2023-02-17 18:46:16 -0500

    update SafetyBeacon comments, fix duty cycle code

--- workshop-example-sketches/Bluefruit_SafteyBeacon_v1.1.py
+++ workshop-example-sketches/Bluefruit_SafteyBeacon_v1.1.py
@@ -4,35 +4,34 @@ University of Pittsburgh Swanson School of Engineering
 v1.1 Amelia Glenn 2/11/2022
 
 Last modified:
-Benjamin Esquieres 10/14/2022
+Benjamin Esquieres 02/17/2023
 Changes:
-Formatting and comments
+Formatting and comments, fix duty cyle code
 """
 
-import board  # import the pins on this board
-from analogio import AnalogIn  # import access to analog input from basic analog IO
-import pwmio  # import access to basic PWM IO
+import board  # import board module to access board's pins
+from analogio import AnalogIn  # import AnalogIn from analogio module to access analog IO
+import pwmio  # import pwmio module to generate Pulse-width Modulation signals
 
-anin = AnalogIn(board.A3)  # anin is an analog input connected to pin A3
-pwmout = pwmio.PWMOut(board.A2)  # pwmout is a PWM output connected to pin A2
+analog_input = AnalogIn(board.A3)  # analog_input is an analog input connected to pin A3
+pwm_output = pwmio.PWMOut(board.A2)  # pwm_output is a PWM output (default frequency of 500Hz) connected to pin A2
 
 """The digitized value of the analog input ranges from 0 to 65535.
 (The Bluefruit has 16-bit analog-to-digital conversion, yielding 65536 unique digital values.)
-Note that this output is not "true analog": it will be a pulse width modulated square wave (PWM)."""
+Note that analog output is not "true analog": it will be a pulse width modulated (PWM) square wave."""
+MAX_ANALOG_VOLTAGE = 65535
 
 while True:  # loop until program terminates, analogous to void loop() in Arduino
     "Write the digitized analog input value to the serial monitor. To view: Ctrl+Shift+M (PC) or Cmd+Shift+M (Mac)"
 
     """Light brightens"""
-    for i in range(0, 65535, 10):  # for i = 0, 10, 20, ... , 65530
-        duty_cycle = (
-            anin.value * i
-        ) / 100000  # multiply the value of anin by i and divide by 100000 to calculate PWM duty cycle
-        pwmout.duty_cycle = int(duty_cycle)  # set pwmout duty cycle, which sets the brightness of the LED
-        print(duty_cycle)  # print duty cycle to serial monitor
+    for i in range(0, MAX_ANALOG_VOLTAGE, 5):  # for i = 0, 5, 10, ... , 65530
+        duty_cycle = int((analog_input / MAX_ANALOG_VOLTAGE) * i)  # scale i by the voltage level reading from analog_input to calculate PWM duty cycle
+        pwm_output.duty_cycle = duty_cycle  # set pwm_output duty cycle to set the brightness of the LED (increase duty cycle to increase brightness)
+        print(f"Duty Cycle: {duty_cycle} ({int(duty_cycle / MAX_ANALOG_VOLTAGE)}%)")  # print duty cycle to serial monitor
 
     """Light dims"""
-    for i in range(65535, 0, -10):  # for i = 65535, 65525, 65515, ... , 5
-        duty_cycle = (anin.value * i) / 100000
-        pwmout.duty_cycle = int(duty_cycle)
-        print(duty_cycle)
+    for i in range(MAX_ANALOG_VOLTAGE, 0, -5):  # for i = 65535, 65530, 65525, ... , 5
+        duty_cycle = int((analog_input / MAX_ANALOG_VOLTAGE) * i)
+        pwm_output.duty_cycle = duty_cycle
+        print(f"Duty Cycle: {duty_cycle} ({int(duty_cycle / MAX_ANALOG_VOLTAGE)}%)")
