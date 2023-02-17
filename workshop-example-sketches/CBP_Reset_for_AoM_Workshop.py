895b64c9d527e3402b252734b3bc070bd499ec0a
delta-12 <btesq235@gmail.com>
2023-02-17 18:49:02 -0500

    update CPB reset for workshop formatting

--- workshop-example-sketches/CBP_Reset_for_AoM_Workshop.py
+++ workshop-example-sketches/CBP_Reset_for_AoM_Workshop.py
@@ -1,12 +1,18 @@
-'''CPB_Reset_for_AoM_Workshop
+"""CPB_Reset_for_AoM_Workshop
 Resets the Circuit Playground so it's a blank slate and ready for the first exercise of the workshop.
 Developed for The Art of Making: An Introduction to Hands-On System Design and Engineering
 University of Pittsburgh Swanson School of Engineering
-v1.0 Amelia Glenn 2/14/2022'''
+v1.0 Amelia Glenn 2/14/2022
+
+Last modified:
+Benjamin Esquieres 02/17/2023
+Changes:
+Formatting
+"""
 
 import board
 import neopixel
 
-onboard=neopixel.NeoPixel(board.D8,10,brightness=.5)
+onboard = neopixel.NeoPixel(board.D8, 10, brightness=0.5)
 while True:
-    onboard.fill((0,0,0))
+    onboard.fill((0, 0, 0))
