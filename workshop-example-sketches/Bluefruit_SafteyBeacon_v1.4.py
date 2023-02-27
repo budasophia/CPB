ed3bf804197369e7b020e595f2c9a4c4c85b37c9
delta-12 <btesq235@gmail.com>
2023-02-27 11:49:30 -0500

    replace int cast in SafetyBeacon print duty cycle with float print formatting

--- workshop-example-sketches/Bluefruit_SafteyBeacon_v1.4.py
+++ workshop-example-sketches/Bluefruit_SafteyBeacon_v1.4.py
@@ -1,14 +1,14 @@
-""" Bluefruit_SafteyBeacon_v1.3.py
+""" Bluefruit_SafteyBeacon_v1.4.py
 Developed for The Art of Making: An Introduction to Hands-On System Design and Engineering
 University of Pittsburgh Swanson School of Engineering
 v1.1 Amelia Glenn 2/11/2022
 
-Latest version: v1.3
+Latest version: v1.4
 
 Last modified:
-Benjamin Esquieres 02/17/2023
+Benjamin Esquieres 02/27/2023
 Changes:
-Formatting and comments, fix duty cycle code
+Replace int cast with float print formatting
 """
 
 import board  # import board module to access board's pins
@@ -30,10 +30,10 @@ while True:  # loop until program terminates, analogous to void loop() in Arduin
     for i in range(0, MAX_ANALOG_VOLTAGE, 15):  # for i = 0, 5, 10, ... , 65530
         duty_cycle = int((analog_input.value / MAX_ANALOG_VOLTAGE) * i)  # scale i by the voltage level reading from analog_input to calculate PWM duty cycle
         pwm_output.duty_cycle = duty_cycle  # set pwm_output duty cycle to set the brightness of the LED (increase duty cycle to increase brightness)
-        print(f"Duty Cycle: {duty_cycle} ({int(duty_cycle * 100 / MAX_ANALOG_VOLTAGE)}%)")  # print duty cycle to serial monitor
+        print(f"Duty Cycle: {duty_cycle} ({(duty_cycle * 100 / MAX_ANALOG_VOLTAGE):.0f}%)")  # print duty cycle to serial monitor
 
     """Light dims"""
     for i in range(MAX_ANALOG_VOLTAGE, 0, -15):  # for i = 65535, 65530, 65525, ... , 5
         duty_cycle = int((analog_input.value / MAX_ANALOG_VOLTAGE) * i)
         pwm_output.duty_cycle = duty_cycle
-        print(f"Duty Cycle: {duty_cycle} ({int(duty_cycle * 100 / MAX_ANALOG_VOLTAGE)}%)")
+        print(f"Duty Cycle: {duty_cycle} ({(duty_cycle * 100 / MAX_ANALOG_VOLTAGE):.0f}%)")
