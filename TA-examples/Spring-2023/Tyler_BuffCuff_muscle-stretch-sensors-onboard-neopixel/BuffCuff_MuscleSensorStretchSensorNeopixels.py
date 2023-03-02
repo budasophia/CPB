82c34a774773c6972ac07e5c8b32fd2dda0e1d88
thans28 <122710485+thans28@users.noreply.github.com>
2023-03-02 11:59:41 -0500

    Added comments and constants

--- TA-examples/Spring-2023/Tyler_BuffCuff_muscle-stretch-sensors-onboard-neopixel/BuffCuff_MuscleSensorStretchSensorNeopixels.py
+++ TA-examples/Spring-2023/Tyler_BuffCuff_muscle-stretch-sensors-onboard-neopixel/BuffCuff_MuscleSensorStretchSensorNeopixels.py
@@ -15,18 +15,24 @@ from analogio import AnalogIn
 import neopixel
 import time
 
-muscleIn = AnalogIn(board.A4)
-stretchIn = AnalogIn(board.A3)
+#configure analog inputs
+muscleIn = AnalogIn(board.A4)  #myoware muscle sensor
+stretchIn = AnalogIn(board.A3)  #conductive rubber cord sensor
 
+#configure onboard neopixels
 onboard = neopixel.NeoPixel(board.D8, 10, brightness=0.5)
+
+#establish constants to be used in while loop
+muscleThreshold = 60000  #value you choose to set how much you flex before the program reacts to it
+stretchThreshold = 55000  #value you choose to set how much you stretch the rubber cord before the program reacts to it
 
 while True:
 
-    if muscleIn.value > 60000:
-        onboard.fill((0, 255, 0))
-    elif stretchIn.value < 55000:
-        onboard.fill((0, 0, 255))
+    if muscleIn.value > muscleThreshold:
+        onboard.fill((0, 255, 0))  #onboard neopixels set to completely green
+    elif stretchIn.value < stretchThreshold:
+        onboard.fill((0, 0, 255))  #onboard neopixels set to completely blue
     else:
-        onboard.fill((0, 0, 0))
+        onboard.fill((0, 0, 0))  #onboard neopixels turned off
 
-    time.sleep(0.1)
+    time.sleep(0.5)  #delay to account for minor fluctuations
