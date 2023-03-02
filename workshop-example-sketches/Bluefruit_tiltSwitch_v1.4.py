560cd296a0a7496e16de2566b8cdb46f31a0aae9
delta-12 <btesq235@gmail.com>
2023-03-02 12:17:50 -0500

    remove input pull direction for tilt switch

--- workshop-example-sketches/Bluefruit_tiltSwitch_v1.4.py
+++ workshop-example-sketches/Bluefruit_tiltSwitch_v1.4.py
@@ -1,14 +1,14 @@
-""" Bluefruit_tiltSwitch_v1.3.py
+""" Bluefruit_tiltSwitch_v1.4.py
 Developed for The Art of Making: An Introduction to Hands-On System Design and Engineering
 University of Pittsburgh Swanson School of Engineering
 v1.1 Amelia Glenn 2/13/2022
 
-Latest version: v1.3
+Latest version: v1.4
 
 Last modified:
-Benjamin Esquieres 02/17/2023
+Benjamin Esquieres 03/02/2023
 Changes:
-Formatting and comments
+Remove input pull direction
 """
 
 import board  # import board module to access board's pins
@@ -19,9 +19,7 @@ led = digitalio.DigitalInOut(board.D9)  # LED is connected to pin D9
 led.switch_to_output()  # LED is a digital output
 
 tiltball = digitalio.DigitalInOut(board.D10)  # tilt switch is conneted to pin D10
-tiltball.switch_to_input(
-    pull=digitalio.Pull.UP
-)  # tiltball is a digital input and is initially off
+tiltball.switch_to_input()  # tiltball is a digital input
 
 while True:  # loop until program terminates, analogous to void loop() in Arduino
     if tiltball.value == True:  # if tilt switch is vertical (closed circuit)
